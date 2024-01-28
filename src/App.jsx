8483158ae540ec7662cb5f47a92c66d38ddba2cc
aleksname <aleksname.v2@gmail.com>
2024-01-28 09:36:34 +0200

    change: cleaned code

--- src/App.jsx
+++ src/App.jsx
@@ -9,8 +9,6 @@ import { differences } from './data'
 function App() {
   const [contentType, setContenttype] = useState(null)
 
-  console.log('App render');
-
   function handleClick(type){
     setContenttype(type)
     console.log(contentType);
@@ -29,23 +27,15 @@ function App() {
             {ways.map((way) =>(
               <ReturnElementLi key={way.title} {...way}/>
             ))}
-
-            {/* <ReturnElementLi title={ways[0].title} description={ways[0].description}/>
-            <ReturnElementLi title={ways[1].title} description={ways[1].description}/>
-            <ReturnElementLi title={ways[2].title} description={ways[2].description}/>
-            <ReturnElementLi title={ways[3].title} description={ways[3].description}/> */}
           </ul>
         </section>
         <section>
           <h3>Lorem ipsum dolor sit amet</h3>
-      
           <Button isActive={contentType === 'way'} onClick={() => handleClick("way")}>Button_1</Button>
           <Button isActive={contentType === 'easy'} onClick={() => handleClick("easy")}>Button_2</Button>
           <Button isActive={contentType === 'program'} onClick={() => handleClick("program")}>Button_3</Button>
-
           { !contentType ?  <p>Touch it</p> : null}
           { contentType ?  <p>{differences[contentType]}</p> : null}
-         
         </section>
       </main>
     </div> 
