ec230d773401f0e9e58bc377db09d361ca55123b
aleksname <aleksname.v2@gmail.com>
2024-02-14 15:48:10 +0200

    change: refresh code

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,20 +1,28 @@
 import { useState } from 'react'
 import logo from '/golova.png'
-import classes from './Header.module.css'
+import  './Header.css'
+import {styled } from 'styled-components'
 
-console.log(classes);
+const HeaderComponent = styled.header`
+   display: flex;
+    justify-content: space-between;
+    align-items: center;
+    padding: 10px 5px 10px 5px;
+    border-bottom: 2px solid #0000007d; 
+`
+
 function Header() {
   const [now, setNow] = useState(new Date())
 
   setInterval(() => setNow(new Date()),1000)
     return (
-    <header>
+    <HeaderComponent>
         <div className="header__block-logo">
             <p className="header__text">My Portfolio!</p>
             <img src={logo} alt="logo" className='header__logo' />
         </div>
           <span>Time: {now.toLocaleTimeString()}</span>
-        </header>
+        </HeaderComponent>
     )
   }
 
